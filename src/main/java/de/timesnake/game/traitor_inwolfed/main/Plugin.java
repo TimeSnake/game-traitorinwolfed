ea418242f1638f2c8b0e4f7f12311bb0773adcb9
funzter <cavehive@protonmail.com>
2024-05-17 21:41:53 +0200

    minor

--- src/main/java/de/timesnake/game/traitor_inwolfed/main/Plugin.java
+++ src/main/java/de/timesnake/game/traitor_inwolfed/main/Plugin.java
@@ -4,16 +4,11 @@
 
 package de.timesnake.game.traitor_inwolfed.main;
 
-import de.timesnake.library.basic.util.LogHelper;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Plugin extends de.timesnake.basic.game.util.user.Plugin {
 
-  public static final Plugin TRAITOR_INWOLFED = new Plugin("TraitorInwolfed", "GTI",
-      LogHelper.getLogger("TraitorInwolfed", Level.INFO));
+  public static final Plugin TRAITOR_INWOLFED = new Plugin("TraitorInwolfed", "GTI");
 
-  protected Plugin(String name, String code, Logger logger) {
-    super(name, code, logger);
+  protected Plugin(String name, String code) {
+    super(name, code);
   }
 }
